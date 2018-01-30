20fd6d79bdc8e89c4a97afaf286fff3d1e05d99f
LilyVL <s098448@ap.be>
2018-01-30 14:49:58 +0100

    Added posts & search results

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Listening on port 3000');
+var request = require('request');
+
+const posturl = 'http://jsonplaceholder.typicode.com/posts'
+const commenturl = 'http://jsonplaceholder.typicode.com/comments'
+
+
+app.listen(process.env.PORT || 3010, () => {
+  console.log('Listening on port 3010');
+
 });
 
 app.set('view engine', 'ejs');
@@ -12,17 +19,28 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // Redirect to posts
-app.get('/', (req, res) => {
-   res.redirect('/posts');
+app.get('/', (req, res) => {	  
+  res.redirect('/posts'); 
 });
 
 // List all posts
-app.get('/posts', (req, res) => {});
+app.get('/posts', (req, res) => {
+	request(posturl, function (error, response, body) {
+		var posts = JSON.parse(body)
+  	res.render('posts.ejs', {posts})
+});
+});
 
 // Show the search form
 app.get('/search', (req, res) => {
-   res.render('search.ejs', { post: '' });
+   res.render('search.ejs', { post: 'name' });
 });
 
 // Find all comments for post
-app.post('/search', (req, res) => {});
+app.post('/search', (req, res) => {
+	request(commenturl, function (error, response, body) {
+		var comments = JSON.parse(body)
+		var name = req.body.title
+  	res.render('search_result.ejs', {comments, name})
+});
+});
